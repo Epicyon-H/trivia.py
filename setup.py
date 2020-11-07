01cf0fbe0cef7d1348f7e182e00f85414866e150
Epicyon-H <64486098+Epicyon-H@users.noreply.github.com>
2020-11-07 16:21:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="trivia.py",
-    version="1.0.0",
+    version="1.0.1",
     description="A python wrapper for Open Trivia DB",
     long_description=README,
     long_description_content_type="text/markdown",
